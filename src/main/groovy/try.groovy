9ce70d08b024786ef00283894d00ac6d73f72753
danielg <danielg@jfrog.com>
2020-06-12 09:45:29 +0300

    Work in progress

--- src/main/groovy/try.groovy
+++ src/main/groovy/try.groovy
@@ -1,7 +1,10 @@
 import com.sun.net.httpserver.HttpServer
+import groovy.json.*
 
 int PORT = 3000
-String filePath = "./votes.json"
+
+def jsonSlurper = new JsonSlurper()
+resutlsFile = jsonSlurper.parse(new File("./votes.json"))
 
 
 def writeResultsToFile(String Path, String textToWrite){
@@ -9,24 +12,33 @@ def writeResultsToFile(String Path, String textToWrite){
     boolean isExists =  file.exists()
     if(isExists) {
         file.write(textToWrite)
-        }
-        else {
-            new File(Path).write(textToWrite)
-        }
     }
+    else {
+        new File(Path).write(textToWrite)
+    }
+}
 
 
 HttpServer.create(new InetSocketAddress(PORT), /*max backlog*/ 0).with {
     println "Server is listening on ${PORT}, hit Ctrl+C to exit."
-    createContext("/") { http ->
-        http.responseHeaders.add("Content-type", "text/plain")
+    createContext("/vote") { http ->
+        http.responseHeaders.add("Content-type", "text/html")
         http.sendResponseHeaders(200, 0)
         http.responseBody.withWriter { out ->
             out << "Your vote is accepted\n"
             String requestBody = http.requestBody.text.toString()
-            writeResultsToFile(filePath, requestBody)
+            writeResultsToFile("./votes.json", requestBody)
         }
-        println "Request received"
+        println ("${http.requestMethod.toString()} Request received")
+    }
+
+    createContext("/candidates") { http ->
+        http.responseHeaders.add("Content-type", "text/html")
+        http.sendResponseHeaders(200, 0)
+        http.responseBody.withWriter { out ->
+            out << resutlsFile
+        }
+        println ("${http.requestMethod.toString()} Request received")
     }
     start()
 }
